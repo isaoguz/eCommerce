d0549fc6fbde429ec38ca7dd003a6573ec8ce03e
isaoguz <isa34oguz@gmail.com>
2023-04-16 20:19:22 +0300

    Sayfaların Oluşturulması

--- resources/views/anasayfa.blade.php
+++ resources/views/anasayfa.blade.php
@@ -1,71 +1,160 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-    </head>
-    <body>
-    @php
-    $yas = 29;
-    @endphp
-
-    Merhaba {{$isim . ' ' . $soyisim}} , {{$yas}} yasındasın. <br>
-<hr>
-
-    @if($isim=='isa')
-        Hoşgledin Patron <br>
-    @elseif($isim=='baskan')
-        Hoşgeldin Başkan <br>
-    @else
-        Hoşgeldin <br>
-    @endif
-<hr>
-    @switch($isim)
-        @case('baskan')
-        Hoşgeldin baskan <br>
-        @break
-
-        @case('isa')
-        Hoşgeldin İsa <br>
-        @break
-
-    @endswitch
-
-<hr>
-    @for($i=0;$i<10; $i++)
-        Döngü Değeri : {{$i}}
-    @endfor
-
-    <hr>
-    @php
-    $i=0
-    @endphp
-    @while($i<10)
-        Döngü Değeri : {{$i}}
-        @php
-        $i++;
-        @endphp
-    @endwhile
-
-    <hr>
-    @foreach($isimler as $isim)
-    {{$isim . ($isim !==end($isimler) ? ',' : '')}}
-    @endforeach
-<hr>
-    @foreach($kullanicilar as $kullanici)
-        @continue($kullanici['id']==1)
-        <li>{{$kullanici['id'] . '-' . $kullanici['kullanici_adi']}}</li>
-        @break($kullanici['id']==4)
-    @endforeach
-
-
-    </body>
-</html>
+@extends('layouts.master')
+@section('title','Ofis eşyaları satış')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Kategoriler</div>
+                    <div class="list-group categories">
+                        <a href="#" class="list-group-item"><i class="fa fa-television"></i> Kategori</a>
+                        <a href="#" class="list-group-item"><i class="fa fa-television"></i> Kategori</a>
+                        <a href="#" class="list-group-item"><i class="fa fa-television"></i> Kategori</a>
+                        <a href="#" class="list-group-item"><i class="fa fa-television"></i> Kategori</a>
+                        <a href="#" class="list-group-item"><i class="fa fa-television"></i> Kategori</a>
+                        <a href="#" class="list-group-item"><i class="fa fa-television"></i> Kategori</a>
+                        <a href="#" class="list-group-item"><i class="fa fa-television"></i> Kategori</a>
+                        <a href="#" class="list-group-item"><i class="fa fa-television"></i> Kategori</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <img src="http://lorempixel.com/640/400/food/1" alt="...">
+                            <div class="carousel-caption">
+                                Slide 1
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="http://lorempixel.com/640/400/food/2" alt="...">
+                            <div class="carousel-caption">
+                                Slide 2
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="http://lorempixel.com/640/400/food/3" alt="...">
+                            <div class="carousel-caption">
+                                Slide 3
+                            </div>
+                        </div>
+                    </div>
+                    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="panel panel-default" id="sidebar-product">
+                    <div class="panel-heading">Günün Fırsatı</div>
+                    <div class="panel-body">
+                        <a href="#">
+                            <img src="http://lorempixel.com/400/485/food/1" class="img-responsive">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="products">
+            <div class="panel panel-theme">
+                <div class="panel-heading">Öne Çıkan Ürünler</div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-md-3 product">
+                            <a href="#"><img src="http://lorempixel.com/400/400/food/1"></a>
+                            <p><a href="#">Ürün adı</a></p>
+                            <p class="price">129 ₺</p>
+                        </div>
+                        <div class="col-md-3 product">
+                            <a href="#"><img src="http://lorempixel.com/400/400/food/2"></a>
+                            <p><a href="#">Ürün adı</a></p>
+                            <p class="price">129 ₺</p>
+                        </div>
+                        <div class="col-md-3 product">
+                            <a href="#"><img src="http://lorempixel.com/400/400/food/3"></a>
+                            <p><a href="#">Ürün adı</a></p>
+                            <p class="price">129 ₺</p>
+                        </div>
+                        <div class="col-md-3 product">
+                            <a href="#"><img src="http://lorempixel.com/400/400/food/4"></a>
+                            <p><a href="#">Ürün adı</a></p>
+                            <p class="price">129 ₺</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="products">
+            <div class="panel panel-theme">
+                <div class="panel-heading">Çok Satan Ürünler</div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-md-3 product">
+                            <a href="#"><img src="http://lorempixel.com/400/400/food/1"></a>
+                            <p><a href="#">Ürün adı</a></p>
+                            <p class="price">129 ₺</p>
+                        </div>
+                        <div class="col-md-3 product">
+                            <a href="#"><img src="http://lorempixel.com/400/400/food/2"></a>
+                            <p><a href="#">Ürün adı</a></p>
+                            <p class="price">129 ₺</p>
+                        </div>
+                        <div class="col-md-3 product">
+                            <a href="#"><img src="http://lorempixel.com/400/400/food/3"></a>
+                            <p><a href="#">Ürün adı</a></p>
+                            <p class="price">129 ₺</p>
+                        </div>
+                        <div class="col-md-3 product">
+                            <a href="#"><img src="http://lorempixel.com/400/400/food/4"></a>
+                            <p><a href="#">Ürün adı</a></p>
+                            <p class="price">129 ₺</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="products">
+            <div class="panel panel-theme">
+                <div class="panel-heading">İndirimli Ürünler</div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-md-3 product">
+                            <a href="#"><img src="http://lorempixel.com/400/400/food/1"></a>
+                            <p><a href="#">Ürün adı</a></p>
+                            <p class="price">129 ₺</p>
+                        </div>
+                        <div class="col-md-3 product">
+                            <a href="#"><img src="http://lorempixel.com/400/400/food/2"></a>
+                            <p><a href="#">Ürün adı</a></p>
+                            <p class="price">129 ₺</p>
+                        </div>
+                        <div class="col-md-3 product">
+                            <a href="#"><img src="http://lorempixel.com/400/400/food/3"></a>
+                            <p><a href="#">Ürün adı</a></p>
+                            <p class="price">129 ₺</p>
+                        </div>
+                        <div class="col-md-3 product">
+                            <a href="#"><img src="http://lorempixel.com/400/400/food/4"></a>
+                            <p><a href="#">Ürün adı</a></p>
+                            <p class="price">129 ₺</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 /*Route::get('/', function () {
     return view('anasayfa');
 });*/
-
+/*
 
 Route::get('/merhaba',function (){
     return "Merahaba";
@@ -32,8 +32,20 @@ Route::get('/urun/{urunAdi}/{id?}',function ($urunAdi,$id=0){
 
 Route::get('/kampanya',function (){
     return redirect()->route('urun_detay',['urunAdi'=>'elma','id'=>5]);
-});
+});*/
 
 
 //Controller ile kullanımı
 Route::get('/','AnasayfaController@index')->name('anasayfa');
+Route::get('/kategori/{slug_kategoriadi}' , 'KategoriController@index' )->name('kategori');
+Route::get('/urun/{slug_urunadi}','UrunController@index')->name('urun');
+Route::get('/sepet', 'SepetController@index')->name('sepet');
+Route::get('/odeme','OdemeController@index')->name('odeme');
+Route::get('/siparisler' ,'SiparisController@index')->name('siparisler');
+Route::get('/siparisler/{id}','SiparisController@detay')->name('siparis');
+
+Route::group(['prefix'=>'kullanici'],function (){
+    Route::get('/oturumac','KullaniciController@giris_form')->name('kullanici.oturumac');
+    Route::get('/kaydol','KullaniciController@kaydol_form')->name('kullanici.kaydol');
+});
+
